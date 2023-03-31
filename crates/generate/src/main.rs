8715f000a3299ab36d7367011c9db384a2051c47
Flix <flix@willi.cyou>
2023-03-31 20:28:48 +0200

    refactor: Use own FHIR models for code generation

--- crates/generate/src/main.rs
+++ crates/generate/src/main.rs
@@ -1,10 +1,14 @@
 //! Run code generation.
+#![allow(clippy::print_stdout)]
 
 use anyhow::Result;
 
 fn main() -> Result<()> {
+	println!("Generating R4B models..");
 	generate::generate_code("r4b")?;
+	println!("Generating R5 models..");
 	generate::generate_code("r5")?;
 
+	println!("Done.");
 	Ok(())
 }
